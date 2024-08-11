155feb9fd73c0f18dbbc79e682fd4e5e5b9f6275
ritik <flipit001@gmail.com>
2024-08-10 17:48:17 -0700

    moved TYPES_OF_LETTERS to constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,5 @@
 import random
 import os
 SEED = int.from_bytes(os.urandom(8), byteorder="big")
+TYPES_OF_LETTERS = tuple(["ascii_lowercase", "ascii_uppercase", "digits", "punctuation"])
 random.seed(SEED)
--- src/generate_password.py
+++ src/generate_password.py
@@ -2,12 +2,10 @@ from constants import *
 import random
 import string
 
-_TYPES_OF_LETTERS = tuple(["ascii_lowercase", "ascii_uppercase", "digits", "punctuation"])
-
 def generate_password(length: int = 14) -> str:
     password_structure = []
     for i in range(length):
-        password_structure.append(_TYPES_OF_LETTERS[i % 4])
+        password_structure.append(TYPES_OF_LETTERS[i % 4])
 
     random.shuffle(password_structure)
 
